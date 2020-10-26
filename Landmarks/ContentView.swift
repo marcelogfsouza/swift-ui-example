2feb696f92f895aacfdeb072076bdef41f439c1e
SOUZA, Marcelo (MTL) <Marcelo.SOUZA2@bdc.ca>
2020-10-26 10:15:54 -0400

    Created the project

---
+++ Landmarks/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Landmarks
+//
+//  Created by SOUZA, Marcelo (MTL) on 2020-10-26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, SwiftUI!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
