6ab22da9064443aa254a39e0bac8c4dc50806388
SOUZA, Marcelo (MTL) <Marcelo.SOUZA2@bdc.ca>
2020-10-26 10:15:54 -0400

    Created and Combined Views

---
+++ Landmarks/ContentView.swift
@@ -0,0 +1,41 @@
+//
+//  ContentView.swift
+//  Landmarks
+//
+//  Created by SOUZA, Marcelo (MTL) on 2020-10-26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            MapView()
+                .frame(height: 300)
+            
+            CircleImage()
+                .offset(y: -130)
+                .padding(.bottom, -130)
+            
+            VStack(alignment: .leading) {
+                Text("Turtle Rock")
+                    .font(.title)
+                HStack {
+                    Text("Joshua Tree National Park")
+                        .font(.subheadline)
+                    Spacer()
+                    Text("California")
+                        .font(.subheadline)
+                }
+            }
+            .padding()
+            Spacer()
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
